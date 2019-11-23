77b3158141409011325f3600121105a5f123b2a6
Oscar Ramirez <oscar@rinro.com.mx>
2019-11-23 17:44:40 -0600

    Partner fields

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'PARTNER FIELDS',
+    'version': '12.0.0.1.0',
+    'author': 'Oscar Ramirez',
+    'website': 'https://github.com/HomebrewSoft/partner_fields.git',
+    'depends': [
+        'fecr'
+    ],
+    'data': [
+        'views/res_partner_view.xml'
+    ],
+    'qweb': [
+    ],
+}
---
+++ models/res_partner.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    activity_id = fields.Many2many(
+        comodel_name='economic_activity',
+        string='Actividad Económica',
+        required=True,
+    )
+    barrio = fields.Char(string='Barrio', size=75)
+    referencia = fields.Char(string='Referencia', size=125)
+    phone2 = fields.Char(string='Teléfono 2', size=24)
+    mobile2 = fields.Char(string='Móvil 2', size=24)
+    cia_tag_id = fields.Char(
+        string='Etiquetas de la Cia',
+        related="parent_id.name",
+        searchable=True
+    )
